cdf5e949ae073c6c084ae3249c6bc25627226bd3
ajzal-byte <ajzalaju10@gmail.com>
2024-02-22 01:20:26 +0530

    Special Offer Sectin updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,14 @@
-const Button = ({label, iconURL}) => {
+const Button = ({label, iconURL, backgroundColor, textColor, borderColor}) => {
   return (
-    <button className="flex items-center justify-center gap-2 py-4 text-lg leading-none text-white border rounded-full px-7 font-montserrat bg-coral-red border-coral-red hover:bg-white hover:text-coral-red">
+    <button className={`flex items-center justify-center gap-2 py-4 text-lg leading-none border px-7 font-montserrat 
+    ${
+      backgroundColor
+       ? `${backgroundColor} ${textColor} ${borderColor}` 
+       : "text-white bg-coral-red border-coral-red hover:bg-white hover:text-coral-red" }
+    rounded-full 
+    `}>
       {label}
+
       {iconURL && <img src={iconURL} alt="arrow right icon"
       className="hover:border hover:border-coral-red w-5 h-5 ml-2 rounded-full" />}
     </button>
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,6 +1,30 @@
+import { arrowRight } from "../assets/icons"
+import { offer } from "../assets/images"
+import Button from "../components/Button"
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img src={offer} alt="" width={773} height={687} className="object-contain w-full" />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className='font-palanquin text-4xl capitalize font-bold lg:max-w-lg'>
+          <span className='text-coral-red'> Special
+          </span> Offer 
+          </h2>
+          <p className='mt-4 lg:max-w-lg info-text'>
+          Let your shoes do the talking as you walk tall in our exclusive range that promises to elevate your look and elevate your confidence.
+          </p>
+          <p className="mt-6 lg:max-w-lg info-text">From casual cool to formal finesse, our shoes are designed to complement your individuality and add a touch of sophistication to every step you take.</p>
+          <div className="mt-11 flex flex-wrap gap-4">
+            <Button label="Shop Now" iconURL={arrowRight}/>
+            <Button label="Learn More"
+            backgroundColor="bg-white"
+            borderColor=" border-slate-gray"
+            textColor="text-slate-gray" />
+          </div>
+      </div>
+    </section>
   )
 }
 
