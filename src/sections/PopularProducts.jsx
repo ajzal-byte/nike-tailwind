465c5e95666d4ef6ca2c85827c03fec06e18edfd
ajzal-byte <ajzalaju10@gmail.com>
2024-02-21 18:22:28 +0530

    Popular Products Section updated

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,6 +1,21 @@
+import PopularProductCard from '../components/PopularProductCard';
+import {products} from '../constants';
 const PopularProducts = () => {
   return (
-    <div>PopularProducts</div>
+    <section id='products' 
+    className='max-container max-sm:mt-12'>
+      <div className='flex flex-col justify-start gap-5'>
+    <h2 className='text-4xl font-palanquin font-bold'>
+      Our <span className='text-coral-red'>Popular</span> Products</h2>
+    <p className='lg:max-wlg mt-2 font-montserrat text-slate-gray'>Discover a diverse collection of shoes that blend fashion and function seamlessly, ensuring you step out in confidence every time.</p>
+      </div>
+      <div className='mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-col-2 grid-cols-1 sm:gap-4 gap-14'>
+        {products.map(product=>(
+          <PopularProductCard key={product.name} {...product}
+          />
+        ))}
+      </div>
+    </section>
   )
 }
 
