0266ee0106abcc8d42d1d9f9dc09c1129a863650
ajzal-byte <ajzalaju10@gmail.com>
2024-02-22 02:18:46 +0530

    Subscribe Section updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
-const Button = ({label, iconURL, backgroundColor, textColor, borderColor}) => {
+const Button = ({label, iconURL, backgroundColor, textColor, borderColor, fullWidth }) => {
   return (
     <button className={`flex items-center justify-center gap-2 py-4 text-lg leading-none border px-7 font-montserrat 
     ${
       backgroundColor
        ? `${backgroundColor} ${textColor} ${borderColor}` 
        : "text-white bg-coral-red border-coral-red hover:bg-white hover:text-coral-red" }
-    rounded-full 
+    rounded-full ${fullWidth && 'w-full'}
     `}>
       {label}
 
