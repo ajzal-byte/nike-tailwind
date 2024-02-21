7d8bc6ea1075721e6ce8f37f97635c95c006ea67
ajzal-byte <ajzalaju10@gmail.com>
2024-02-21 19:24:56 +0530

    Super Quality Section updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,8 @@ const Button = ({label, iconURL}) => {
   return (
     <button className="flex items-center justify-center gap-2 py-4 text-lg leading-none text-white border rounded-full px-7 font-montserrat bg-coral-red border-coral-red hover:bg-white hover:text-coral-red">
       {label}
-      <img src={iconURL} alt="arrow right icon"
-      className="hover:border hover:border-coral-red w-5 h-5 ml-2 rounded-full" />
+      {iconURL && <img src={iconURL} alt="arrow right icon"
+      className="hover:border hover:border-coral-red w-5 h-5 ml-2 rounded-full" />}
     </button>
   )
 }
