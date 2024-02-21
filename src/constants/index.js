655eada83228027f3dff5b7a0f29a901cb8eec44
ajzal-byte <ajzalaju10@gmail.com>
2024-02-21 20:10:18 +0530

    Service tagline added

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,6 +5,7 @@ export const navLinks = [
     { href: "#home", label: "Home" },
     { href: "#about-us", label: "About Us" },
     { href: "#products", label: "Products" },
+    { href: "#services", label: "Services" },
     { href: "#contact-us", label: "Contact Us" },
 ];
 
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -2,7 +2,7 @@ import {services} from '../constants';
 import ServiceCard from '../components/ServiceCard';
 const Services = () => {
   return (
-    <section className="max-container flex justify-center flex-wrap gap-9">
+    <section id='services' className="max-container flex justify-center flex-wrap gap-9">
       {services.map((service=>(
         <ServiceCard key={service.label} 
         {...service} />
