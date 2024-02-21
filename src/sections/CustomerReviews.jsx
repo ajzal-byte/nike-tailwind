6619996d39d619d232c15b75b53df0536c1b3ca7
ajzal-byte <ajzalaju10@gmail.com>
2024-02-22 01:54:45 +0530

    Customer Reviews

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,6 +1,28 @@
+import ReviewCard from "../components/ReviewCard";
+import {reviews} from '../constants'
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our
+          <span className="text-coral-red"> Customers</span> Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+      Real Stories, Real Smiles: Hear What Our Customers Have to Say!
+      </p>
+
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map(review=>(
+          <ReviewCard
+          key={review.customerName}
+          imgURL={review.imgURL}
+          customerName={review.customerName}
+          rating={review.rating}
+          feedback={review.feedback} />
+        ))}
+
+      </div>
+    </section>
   )
 }
 
